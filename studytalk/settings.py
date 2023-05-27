dc54083633f5b0ed1c8254fb7819980565fbc28b
Mehfooz Ali <graphicx.mehfooz201@gmail.com>
2023-05-27 23:06:51 +0500

    theme updateing

--- studytalk/settings.py
+++ studytalk/settings.py
@@ -119,6 +119,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+
+#user upload the images
+# STATIC_ROOT = 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
