6429049e8ab06e52a19d95200fff928fae02c2b9
Mehfooz Ali <graphicx.mehfooz201@gmail.com>
2023-06-03 00:26:45 +0500

    topics

--- base/urls.py
+++ base/urls.py
@@ -20,4 +20,8 @@ urlpatterns = [
     # path('search/', views.Search, name='search'),
 
     path('delete-message/<int:pk>/', views.deleteMessage, name='delete-message'),
+
+
+    path('topics/', views.topicsPage, name='topics'),
+    path('activity/', views.activityPage, name='activity'),
 ]
