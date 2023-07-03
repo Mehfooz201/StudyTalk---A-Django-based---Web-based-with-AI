aae8fb58d9174353fc418671dd9431ac565af674
Mehfooz Ali <graphicx.mehfooz201@gmail.com>
2023-07-04 00:16:43 +0500

    update

--- base/views.py
+++ base/views.py
@@ -31,20 +31,20 @@ def loginPage(request):
     if request.user.is_authenticated:
         return redirect('home')
     if request.method=='POST':
-        email = request.POST.get('email').lower()
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
-            user = User.objects.get(email=email)
+            user = User.objects.get(username=username)
         except:
             messages.error(request, "User doest not exist.")
         
-        user = authenticate(request, email=email, password=password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
-            messages.error(request, "Email & Password doest not exist.")
+            messages.error(request, "Ssername & Password doest not exist.")
     context = {'page':page}
     return render(request, 'base/login_register.html', context)
 
